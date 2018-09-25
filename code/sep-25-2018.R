b9b8da84cf501284de79e4dc91d1aa8f01fa202d
Susan Vanderplas <srvanderplas@gmail.com>
2018-09-25 14:01:06 -0500

    Actual code used

--- code/sep-25-2018.R
+++ code/sep-25-2018.R
@@ -1,5 +1,15 @@
-# A quick introduction to the pipe, %>%
+# Homework 4, Q3
 
+library(classdata)
+data(mojo);head(mojo)
+
+library(ggplot2)
+ggplot(data = mojo, aes(x = WeekNo, y = `Total Gross`, group = Title)) + 
+  geom_line() + 
+  facet_wrap(~Year)
+
+# A quick introduction to the pipe, %>%
+library(dplyr)
 a <- 1:10
 a %>% mean()
 mean(a)
@@ -11,290 +21,236 @@ mean(a)
 #   group_by(...) %>%
 #   summarize()
 
-# --- Your Turn ----------------------------------------------------------------
+head(mojo)
+mojo %>% head()
 
-# Warm up: Your turn (04_logical.html, slide 8)
+mojo$Average %>% mean(x = ., na.rm = T)
+
+square <- function(x) x^2
+
+square(mean(a))
+a %>% mean() %>% square()
+
+
+# --- Your Turn ----------------------------------------------------------------
+# Warm up: Your turn (04_logical.html, slide 10)
 # Use the fbi data from the classdata package
 library(classdata)
 library(dplyr)
 library(ggplot2)
-# Get a subset of all crimes in Iowa, Plot incidences/rates for one type of crime over time.
-data(fbi)
-head(fbi)
-tmp <- fbi %>%
-  filter(State == "Iowa", Type == "Murder.and.nonnegligent.Manslaughter")
-ggplot(data = tmp) +
-  geom_line(aes(x = Year, y = Count))
-
-# Get a subset of all crimes in 2009. Plot the number or rate for one type of crime by state.
-
-fbi %>%
-  filter(Year == 2009) %>%
-  filter(Type == "Murder.and.nonnegligent.Manslaughter") %>%
-  ggplot() +
-  geom_col(aes(x = State, y = Count/Population * 100000)) +
-  ylab("Murder rate per 100,000 people") +
-  coord_flip()
-
-# Get a subset of the data that includes number of homicides for the last five years.
-# Find the rate of homicides, extract all states that have a rate of greater
-# than 90% of the rates across all states, and plot (Hint: ?quantile).
-max(fbi$Year)
-tmp <- fbi %>%
-  filter(Year >= 2009) %>%
-  filter(Type == "Murder.and.nonnegligent.Manslaughter") %>%
-  group_by(State) %>%
-  summarize(Count = sum(Count, na.rm = T),
-            Population = mean(Population)) %>%
-  mutate(Rate = Count/Population * 100000)
-
-threshold <- quantile(tmp$Rate, .9, na.rm = T)
-
-filter(tmp, Rate >= threshold) %>%
-  ggplot() +
-  geom_col(aes(x = State, y = Rate)) +
-  ylab("5-year Murder rate per 100,000 people") +
-  coord_flip()
-
-# ------------------------------------------------------------------------------
-
-nrow(fbi)
-
-dim(fbi)
-
-quantile(rnorm(10000), .95)
-quantile(rnorm(10000), c(.9, .95, .99))
-
-which(fbi$Type == "Rape")
-which.max(fbi$Count)
-which.min(fbi$Count)
-
-# --- Your Turn ----------------------------------------------------------------
-
-# Use the fbi data object to answer the following questions:
 # how many reports of Burglaries are there for California?
+data(fbi)
 
-cali_burglary <- filter(fbi, State == "California") %>%
-  filter(Type == "Burglary")
-sum(cali_burglary$Count)
+tmp <- filter(fbi, State == "California", Type == "Burglary")
+sum(tmp$Count)
 
 # for any of the violent crimes, which state had the highest rate (and for which crime) in 2014? in 1961?
 
-violent_2014 <- filter(fbi, Violent.crime == T) %>%
-  filter(Year == 2014)
-
+violent_2014 <- filter(fbi, Year == 2014, Violent.crime == TRUE)
 which.max(violent_2014$Count/violent_2014$Population)
 violent_2014[217,]
 
-violent_1961 <- filter(fbi, Violent.crime == T) %>%
-  filter(Year == 1961)
-
+violent_1961 <- filter(fbi, Year == 1961, Violent.crime == TRUE)
 which.max(violent_1961$Count/violent_1961$Population)
 violent_1961[209,]
 
-# Use the fbiwide data object to answer the following question:
-# in how many states in 2011 were there more vehicle thefts than robberies? (which states are those?)
-
+# in how many states in 2011 were there more vehicle thefts than 
+# robberies? (which states are those?)
 data(fbiwide)
-head(fbiwide)
 
-vehicle_theft_states <- filter(fbiwide, Year == 2011) %>%
-  filter(Motor.vehicle.theft > Robbery)
-
-nrow(vehicle_theft_states)
-vehicle_theft_states$State
+tmp <- fbiwide %>%
+  dplyr::filter(Year == 2011) %>%
+  dplyr::filter(Motor.vehicle.theft > Robbery)
 
 # ------------------------------------------------------------------------------
 
-x1 <- x2 <- sample(1:10, 10, replace = F)
-x1
+a <- 1:4
+a[2:3] <- NA
+a
+a[2:3] <- "NA"
+a
 
-x1[x1 >= 9] <- 0
-x1
 
-replace(x2, x2 >= 9, 0)
-x2
-
-library(classdata)
-# introduces new variable in the data set fbiwide
 fbiwide$murder.rate <- fbiwide$Murder/fbiwide$Population*100000
-names(fbiwide)
+fbiwide[which.max(fbiwide$murder.rate),]
 
-# --- Your Turn ----------------------------------------------------------------
-# The gapminder data from homework 2 is available here.
-# https://raw.githubusercontent.com/Stat579-at-ISU/stat579-at-isu.github.io/master/homework/data/gapminder-5060.csv
-# read.csv helps you read the gapminder data from the given link. Store the result in an object.
-# What format does the object have?
-# Replace the problematic value in life expectancy for Canada in 1957 by the value 69.96.
-
+# --- Your Turn - Slide 13 ----------------------------------------------------
 gapminder <- read.csv("https://raw.githubusercontent.com/Stat579-at-ISU/stat579-at-isu.github.io/master/homework/data/gapminder-5060.csv")
+
+# What format does the object have?
 str(gapminder)
+
+# Replace the problematic value in life expectancy for Canada in 1957 
+# by the value 69.96.
+summary(gapminder$lifeExp)
+which.max(gapminder$lifeExp)
+gapminder[82,]
+which(gapminder$country == "Canada" & gapminder$year == 1957)
+
+gapminder$lifeExp <- replace(gapminder$lifeExp, 
+                             gapminder$country == "Canada" & 
+                               gapminder$year == 1957,
+                             69.96)
+
+gapminder$lifeExp <- replace(gapminder$lifeExp, 
+                             gapminder$lifeExp == 999999, 
+                             69.96)
+gapminder$lifeExp[82] <- 69.96
+
 summary(gapminder$lifeExp)
 
-gapminder$lifeExp[gapminder$lifeExp > 150] <- 69.96
 
-gapminder$lifeExp[which.max(gapminder$lifeExp)] <- 69.96
+#####################################
+# --- Factors -----------------------
 
-
-################################################################################
-# --- Factor Variables ---------------------------------------------------------
-
-x <- sample(letters, 15)
-x_fac <- factor(x, levels = letters)
-x_fac
-x_fac_numeric <- as.numeric(x_fac)
-x_fac_numeric
-
-levels(x_fac)
-
-
-# --- Your Turn ----------------------------------------------------------------
-
-# Inspect the fbi object. How many variables are there? Which type does each of the variables have?
-
-str(fbi) # 7 variables
-# State and Abb are characters
-# Year, Population, and Count are integers
-# Type is a factor
-# Violent.crime is a logical vector
-
-#   Make a summary of Year
-
+# Inspect the fbi object. How many variables are there? 
+# Which type does each of the variables have?
+str(fbi)
+# Make a summary of Year
 summary(fbi$Year)
 
 # Make Year a factor variable: fbi$Year <- factor(fbi$Year)
-
-fbi_copy <- fbi
 fbi$Year <- factor(fbi$Year)
 
 # Compare summary of Year to the previous result
-
 summary(fbi$Year)
 
 # Are there other variables that should be factors (or vice versa)?
-
-# State, Abb should probably be factors
-fbi$State <- factor(fbi$State)
-fbi$Abb <- factor(fbi$Abb)
-
-# --- Factors in Boxplots ------------------------------------------------------
-# Reload fbi data
 data(fbi)
+summary(fbi$State)
+fbi$State <- factor(fbi$State)
+summary(fbi$State)
 
+# ------------------
 twoyear <- dplyr::filter(fbi, Year %in% c(1961, 2014))
 
-ggplot(data = twoyear) + geom_boxplot(aes(x = Year, y = Count))
 
-ggplot(data = twoyear) + geom_boxplot(aes(x = factor(Year), y = Count))
-
-fbi_factor <- fbi
-fbi_factor$Year <- factor(fbi_factor$Year)
-
-str(fbi_factor)
-str(fbi_factor$Year)
-mode(fbi_factor$Year)
-
-is.factor(fbi$Year)
-is.factor(fbi_factor$Year)
-
-is.integer(fbi$Year)
-is.integer(fbi_factor$Year)
-
-is.numeric(fbi$Year)
-is.numeric(fbi_factor$Year)
-
-is.character(fbi$Year)
-is.character(fbi_factor$Year)
-is.character(fbi$State)
-
-is.logical(fbi$Violent.crime)
-is.logical(fbi$Count)
-
-as.character(fbi_factor$Year)
-as.character(fbi$Year)
-as.integer(fbi_factor$Year) # ???
-as.numeric(fbi_factor$Year)
-as.numeric(as.character(fbi_factor$Year)) # What we expected out of as.numeric(factor var)
-
-fbi_factor$Year %>%
-  as.character() %>%
-  as.numeric() # Piped version is much easier to read - sequential instead of inside-out
-
-
-levels(fbi$Type)
-reordered_Type <- factor(
-  fbi$Type,
-  levels = c("Larceny.theft", "Burglary", "Motor.vehicle.theft", "Robbery",
-             "Rape", "Legacy.rape",  "Aggravated.assault",
-             "Murder.and.nonnegligent.Manslaughter"))
-levels(reordered_Type)
-
-
-reorder(fbi$Type, fbi$Count, na.rm = T) %>% levels()
-reorder(fbi$Type, fbi$Count) %>% levels()
-
-
-# --- Your Turn ----------------------------------------------------------------
-
-# For this your turn use the fbi object from the classdata package.
-data(fbi)
-# Introduce a rate of the number of reported offenses by population into the
-# fbi data. You could use the Ames standard to make values comparable to a city
-# of the size of Ames (population ~70,000).
-
-fbi$Rate_Ames <- fbi$Count/fbi$Population * 70000
-
-# Plot boxplots of crime rates by different types of crime. How can you make axis text legible?
-
-fbi %>%
-  ggplot(aes(x = Type, y = Rate_Ames)) +
+ggplot(data = fbi, 
+       aes(x = Year, 
+           y = Count)) + 
   geom_boxplot()
 
-fbi %>%
-  ggplot(aes(x = Type, y = Rate_Ames)) +
-  geom_boxplot() +
-  coord_flip()
+ggplot(data = fbi, 
+       aes(x = factor(Year), 
+           y = Count)) + 
+  geom_boxplot()
 
-fbi$Type <- factor(fbi$Type,
-                   labels = c("Agg. Assault", "Burglary", "Larceny/Theft",
-                              "Legacy Rape", "Vehicle Theft",
-                              "Murder and\nnonnegligent\nmanslaughter",
-                              "Rape", "Robbery"))
-fbi %>%
-  ggplot(aes(x = Type, y = Rate_Ames)) +
-  geom_boxplot() +
-  coord_flip()
 
-#   Reorder the boxplots of crime rates, such that the boxplots are ordered by their medians.
+head(fbi)
 
-fbi$Type <- reorder(fbi$Type, fbi$Rate_Ames, FUN = median)
-fbi %>%
-  ggplot(aes(x = Type, y = Rate_Ames)) +
-  geom_boxplot() +
-  coord_flip()
+is.character(fbi$State)
+str(fbi)
+is.factor(fbi$State)
+is.numeric(fbi$State)
 
-fbi$Type <- reorder(fbi$Type, fbi$Rate_Ames, FUN = median, na.rm = T)
-fbi %>%
-  ggplot(aes(x = Type, y = Rate_Ames)) +
-  geom_boxplot() +
-  coord_flip()
+is.numeric(fbi$Year)
+is.integer(fbi$Count)
+is.logical(fbi$Violent.crime)
 
-# For one type of crime (subset!) plot boxplots of rates by state, reorder boxplots by median crime rates
+as.integer(fbi$Violent.crime)
 
-fbi$State <- factor(fbi$State)
-burgled <- filter(fbi, Type == "Burglary")
+fbi$Year_fac <- factor(fbi$Year)
+as.numeric(fbi$Year_fac)
+as.character(fbi$Year_fac)
+fbi$Year_fac %>% as.character() %>% as.numeric()
+as.numeric(as.character(fbi$Year_fac))
 
-burgled$State <- reorder(burgled$State, burgled$Rate_Ames, FUN = median, na.rm = T)
-burgled %>%
-  ggplot(aes(x = State, y = Rate_Ames)) +
-  geom_boxplot() +
-  coord_flip() +
-  ggtitle("Burglary Rates") +
-  ylab("Annual Burglary Rates per 70,000 people")
+ggplot(data = fbi, 
+       aes(x = Year, 
+           y = Count)) + 
+  geom_col()
 
-# ------------------------------------------------------------------------------
+levels(fbi$State)
 
+levels(fbi$Type)
+fbi$myType <- factor(fbi$Type, labels = c("Assault", "Burglary", 
+                                          "Theft", "Rape1", 
+                                          "Car Theft", "Murder", 
+                                          "Rape2", "Robbery"))
+fbi$myType2 <- factor(fbi$Type, 
+                      levels = c("Aggravated.assault",
+                                 "Burglary",
+                                 "Larceny.theft", 
+                                 "Legacy.rape", 
+                                 "Motor.vehicle.theft", 
+                                 "Murder.and.nonnegligent.Manslaughter",
+                                 "Rape", "Robbery"),
+                      labels = c("Assault", "Burglary", 
+                                 "Theft", "Rape1", 
+                                 "Car Theft", "Murder", 
+                                 "Rape2", "Robbery"))
+
+levels(reorder(fbi$Type, fbi$Count))
+levels(reorder(fbi$Type, fbi$Count, na.rm=TRUE))
+levels(reorder(fbi$Type, fbi$Count, na.rm=TRUE, FUN = min))
+
+
+tmp <- fbi %>% filter(Type == "Murder.and.nonnegligent.Manslaughter")
+min(tmp$Count, na.rm = T) # 1
+tmp <- fbi %>% filter(Type == "Legacy.rape")
+min(tmp$Count, na.rm = T) # 6
+tmp <- fbi %>% filter(Type == "Robbery")
+min(tmp$Count, na.rm = T) # 8
+tmp <- fbi %>% filter(Type == "Aggravated.assault")
+min(tmp$Count, na.rm = T) # 14
+tmp <- fbi %>% filter(Type == "Rape")
+min(tmp$Count, na.rm = T)# 34
+tmp <- fbi %>% filter(Type == "Motor.vehicle.theft")
+min(tmp$Count, na.rm = T) # 178
+tmp <- fbi %>% filter(Type == "Burglary")
+min(tmp$Count, na.rm = T) # 865
+tmp <- fbi %>% filter(Type == "Larceny.theft")
+min(tmp$Count, na.rm = T) # 1489
+
+
+# --- Your Turn ------------------------------------
+# Introduce a rate of the number of reported offenses by population 
+# into the fbi data. You could use the Ames standard to make values 
+# comparable to a city of the size of Ames (population ~70,000).
 data(fbi)
-levels(fbi$Type)
-levels(fbi$Type)[6] <- "Murder"
-levels(fbi$Type)
+
+fbi$rate_ames <- fbi$Count/fbi$Population * 70000
+
+# Plot boxplots of crime rates by different types of crime. How can 
+# you make axis text legible?
+
+ggplot(data = fbi, aes(x = Type, y = rate_ames)) + 
+  geom_boxplot() + 
+  ylab("# Crimes/Year in a city of 70,000") + 
+  coord_flip()
+
+fbi$Type_simple <- factor(fbi$Type, 
+                          levels = c("Aggravated.assault",
+                                     "Burglary",
+                                     "Larceny.theft", 
+                                     "Legacy.rape", 
+                                     "Motor.vehicle.theft", 
+                                     "Murder.and.nonnegligent.Manslaughter",
+                                     "Rape", "Robbery"),
+                          labels = c("Assault", "Burglary", 
+                                     "Theft", "Rape1", 
+                                     "Car Theft", "Murder", 
+                                     "Rape2", "Robbery"))
+ggplot(data = fbi, aes(x = Type_simple, y = rate_ames)) + 
+  geom_boxplot() + 
+  ylab("# Crimes/Year in a city of 70,000") + 
+  coord_flip()
+
+# Reorder the boxplots of crime rates, such that the boxplots are 
+# ordered by their medians.
+
+fbi$Type_simple <- reorder(fbi$Type_simple, fbi$rate_ames, 
+                           FUN = median, na.rm = T)
+ggplot(data = fbi, aes(x = Type_simple, y = rate_ames)) + 
+  geom_boxplot() + 
+  ylab("# Crimes/Year in a city of 70,000")
+
+# For one type of crime (subset!) plot boxplots of rates by state, 
+# reorder boxplots by median crime rates
+
+tmp <- filter(fbi, Type_simple == "Car Theft")
+tmp$State <- reorder(tmp$State, tmp$rate_ames, fun = median, na.rm = T)
+ggplot(data = tmp, aes(x = State, y = rate_ames)) + 
+  geom_boxplot() + 
+  ylab("# Crimes/Year in a city of 70,000") + 
+  coord_flip()
